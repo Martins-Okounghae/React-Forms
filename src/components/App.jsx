de7381e6eb61584adce38ff35633dd80b5d9fb93
Martins-Okuonghae <martins2018.okuonghae@outlook.com>
2021-09-13 17:19:26 +0100

    change react form state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { isFunctionDeclaration } from "typescript";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headerText, setheaderText]= useState("");
+
+  function handleChange(event){
+     setName(event.target.value)
+   
+  }
+
+  function handleClick(event){
+    setheaderText(name)
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello  {headerText}</h1>
+      <form onSubmit={handleClick}> 
+      <input 
+      onChange={handleChange} 
+      type="text" 
+      placeholder="What's your name?"
+      value= {name}
+       />
+      <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
